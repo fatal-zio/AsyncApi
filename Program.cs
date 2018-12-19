a2d44aabc9ad4f89ade9f879e8bb403a62d8e3e6
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-12-19 14:01:56 -0600

    Initial Commit of template code.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
